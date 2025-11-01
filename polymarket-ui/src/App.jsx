621f69cd46ac46d83a305879fe38f58e864442fa
cryptojagatthu <cryptojagatthu@gmail.com>
2025-11-01 11:44:36 +0530

    Create App component with Navbar and MarketGrid

---
+++ polymarket-ui/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import MarketGrid from "./components/MarketGrid";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <MarketGrid />
+    </div>
+  );
+}
+
+export default App;
